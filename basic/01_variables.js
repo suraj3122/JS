449a6c6b11a8938cd7ebb9ec826ea0b4d3ad3eee
suraj3122 <156050387+suraj3122@users.noreply.github.com>
2026-05-31 13:02:17 +0000

    hi

---
+++ basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144533
+let accountEmail = "suraj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId=2 not allowed
+console.log(accountId);
+
+accountEmail="sur@gmail.com"
+accountPassword = "3122"
+accountCity = "Nagar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
