e46ccec1ec8ff12d74885316ae67d58317bd738d
suraj3122 <156050387+suraj3122@users.noreply.github.com>
2026-06-02 07:53:22 +0000

    datatypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as a new JS version .
+
+
+console.log(3+3);
+console.log("Suraj")
+
+console.log(3+3);
+
+// alert(3+3); // we are using node.js , not a browser
+
+let name= "suraj";
+let age = 19
+let isLoggedIn= false
+
+
+//primitve data type 
+//number => 2 to power 53
+//bigint
+//string=> "";
+//boolean => true/false
+// null 
+//undefined =>
+// symbol =>unique 
+
+
+//object
+
+console.log(typeof undefined ); //output undefined , type in JS
+console.log(typeof null);  //output object , error of JS
